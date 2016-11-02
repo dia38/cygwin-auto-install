123cbd3e1880dcaa45b2e0d7687d482197fc36f2
Erich <erich.github@bpisys.com>
2016-11-02 12:59:41 -0500

    Update cygwin-install.bat
    Removed older google code reference to apt-cyg.
    Added lynx to base package for install
    Added download and install for apt-cyg from transcode-open instructions 
    TO DO:
    Update script to select proper installer

--- cygwin-install.bat
+++ cygwin-install.bat
@@ -17,7 +17,7 @@ SET LOCALDIR=%CD%
 SET ROOTDIR=C:/cygwin
  
 REM -- These are the packages we will install (in addition to the default packages)
-SET PACKAGES=mintty,wget,rsync,curl,vim
+SET PACKAGES=bash,mintty,wget,rsync,curl,vim,lynx
 REM -- These are necessary for apt-cyg install, do not change. Any duplicates will be ignored.
 SET PACKAGES=%PACKAGES%,wget,tar,gawk,bzip2,subversion
  
@@ -38,9 +38,14 @@ ECHO.
 
 ECHO apt-cyg installing.
 set PATH=%ROOTDIR%/bin;%PATH%
-%ROOTDIR%/bin/bash.exe -c 'svn --force export http://apt-cyg.googlecode.com/svn/trunk/ /bin/'
+REM -- %ROOTDIR%/bin/bash.exe -c 'svn --force export https://github.com/transcode-open/apt-cyg /bin/'
+REM -- ^                            ^
+REM -- | removed ref to google code |
+REM --   added ref to transcode here 
+%ROOTDIR%/bin/bash.exe -c 'lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg'
+%ROOTDIR%/bin/bash.exe -c 'install apt-cyg /bin'
 %ROOTDIR%/bin/bash.exe -c 'chmod +x /bin/apt-cyg'
-ECHO apt-cyg installed if it says somin like "A    /bin" and "A   /bin/apt-cyg" and "Exported revision 18" or some other number.
+ECHO apt-cyg installed if it says something like "A    /bin" and "A   /bin/apt-cyg" and "Exported revision 18" or some other number.
 
 ENDLOCAL
  
