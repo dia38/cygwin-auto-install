4907986342cea1c62148f2d2cd253fc97f7abca2
Erich <erich.github@bpisys.com>
2016-11-02 12:01:46 -0500

    Update cygwin-install.bat
    Modified packages to suite

--- cygwin-install.bat
+++ cygwin-install.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 REM -- Automates cygwin installation
-REM -- Source: https://github.com/rtwolf/auto-cygwin-install
-REM -- Based on: https://gist.github.com/wjrogers/1016065
+REM -- Source: https://github.com/rotor-head/auto-cygwin-install
+REM -- Based on: https://gist.github.com/wjrogers/1016065 and https://github.com/rtwolf/auto-cygwin-install
  
 SETLOCAL
  
@@ -14,7 +14,7 @@ SET LOCALDIR=%CD%
 SET ROOTDIR=C:/cygwin
  
 REM -- These are the packages we will install (in addition to the default packages)
-SET PACKAGES=mintty,wget,ctags,diffutils,git,git-completion,git-svn,stgit,mercurial
+SET PACKAGES=mintty,wget,rsync,curl,vim
 REM -- These are necessary for apt-cyg install, do not change. Any duplicates will be ignored.
 SET PACKAGES=%PACKAGES%,wget,tar,gawk,bzip2,subversion
  
